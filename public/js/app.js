fb4a06d2fabc5e25be3c4706c8bcb9970090dfd2
Yonathan Castillo <leothan522@gmail.com>
2024-08-21 20:12:52 -0400

    Composer update | Actualizado Show Empresas y Footer

--- public/js/app.js
+++ public/js/app.js
@@ -36,6 +36,9 @@ $(function() {
 });
 
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover({
+        html: true
+    });
 })
 
--- resources/views/dashboard/footer.blade.php
+++ resources/views/dashboard/footer.blade.php
@@ -1,13 +1,15 @@
 <div class="float-right d-none d-sm-block">
     <span class="text-sm">
-        DV.
-        <a href="https://t.me/Leothan" target="_blank">Ing. Yonathan Castillo</a>.
+        DV. &nbsp;<a href="https://t.me/Leothan" target="_blank">Ing. Yonathan Castillo</a>.
     </span>
-    <b>Version</b> 2.0
+    <span>
+        <b>Version</b> 2.0
+    </span>
 </div>
-<strong class="d-none d-sm-block">&copy; 2023 Dirección de Tecnología y Sistemas.
-    <a href="#" class="d-none d-lg-inline-block">Alimentos del Guárico S.A. (Alguarisa).</a>
-</strong>
-<strong class="d-sm-none">
-    &copy; 2023 Alimentos del Guárico S.A.
-</strong>
+<span class="d-none d-sm-block">
+    &copy; 2024 Dirección de Tecnología y Sistemas. &nbsp;
+    <span class="d-none d-lg-inline-block btn-link">Alimentos del Guárico S.A. (Alguarisa).</span>
+</span>
+<span class="d-sm-none btn-link" data-toggle="popover" data-placement="top" title="Developer" data-content="Ing. Yonathan Castillo" style="cursor: pointer;">
+    &copy; 2024 Alimentos del Guárico S.A.
+</span>
