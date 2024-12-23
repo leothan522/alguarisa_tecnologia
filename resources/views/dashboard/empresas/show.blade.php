bdf5f4207a9d8c8f8a0fdfc5cdfdff16bcc2ca10
Yonathan Castillo <leothan522@gmail.com>
2024-12-23 08:19:04 -0400

    Cabios Menores

--- resources/views/dashboard/empresas/show.blade.php
+++ resources/views/dashboard/empresas/show.blade.php
@@ -1,30 +1,30 @@
 <ul class="todo-list list-group list-group-flush" >
     <li class="list-group-item">
-        <span>Nombre</span>
+        <span>Nombre: </span>
         <span class="float-right text-bold text-lightblue text-uppercase">{{ $nombre }}</span>
     </li>
     <li class="list-group-item">
-        <span>RIF</span>
+        <span>RIF: </span>
         <span class="float-right text-bold text-lightblue text-uppercase">{{ $rif }}</span>
     </li>
     <li class="list-group-item">
-        <span>Jefe</span>
+        <span>Jefe: </span>
         <span class="float-right text-bold text-lightblue text-uppercase">{{ $jefe }}</span>
     </li>
     <li class="list-group-item">
-        <span>Moneda Base</span>
+        <span>Moneda Base: </span>
         <span class="float-right text-bold text-lightblue text-uppercase">{{ $moneda }}</span>
     </li>
     <li class="list-group-item">
-        <span>Teléfonos</span>
+        <span>Teléfonos: </span>
         <span class="float-right text-bold text-lightblue text-uppercase">{{ $telefonos }}</span>
     </li>
     <li class="list-group-item">
-        <span>Email</span>
+        <span>Email: </span>
         <span class="float-right text-bold text-lightblue text-lowercase">{{ $email }}</span>
     </li>
     <li class="list-group-item">
-        <span>Dirección</span>
+        <span>Dirección: </span>
         <span class="float-right text-bold text-lightblue text-lowercase">{{ $email }}</span>
     </li>
     @if($empresas_id == $empresaDefault)
